909e82b8d2b19fe1cc836fe1fced7301019d2347
lin3615000 <809692975@qq.com>
2016-11-20 23:17:10 +0800

    增加bootstrap后台管理模板

---
+++ bootstrap/Content/index.php
@@ -0,0 +1,166 @@
+<?php
+if(isset($_POST) && $_POST)
+{
+	print_r($_POST);
+}
+?>
+<style>
+	form {
+		margin: 0;
+	}
+	textarea {
+		display: block;
+	}
+</style>
+		<link rel="stylesheet" href="../editor3615net/themes/default/default.css" />
+		<script charset="utf-8" src="../assets/js/jquery-1.8.1.min.js"></script>
+		<script charset="utf-8" src="../editor3615net/kindeditor-min.js"></script>
+		<script charset="utf-8" src="../editor3615net/lang/zh_CN.js"></script>
+		<script>
+			var editor;
+			KindEditor.ready(function(K) {
+				editor = K.create('textarea[name="content"]', {
+					allowFileManager : true
+				});
+				K('input[name=getHtml]').click(function(e) {
+					alert(editor.html());
+				});
+				K('input[name=isEmpty]').click(function(e) {
+					alert(editor.isEmpty());
+				});
+				K('input[name=getText]').click(function(e) {
+					alert(editor.text());
+				});
+				K('input[name=selectedHtml]').click(function(e) {
+					alert(editor.selectedHtml());
+				});
+				K('input[name=setHtml]').click(function(e) {
+					editor.html('<h3>Hello KindEditor</h3>');
+				});
+				K('input[name=setText]').click(function(e) {
+					editor.text('<h3>Hello KindEditor</h3>');
+				});
+				K('input[name=insertHtml]').click(function(e) {
+					editor.insertHtml('<strong>插入HTML</strong>');
+				});
+				K('input[name=appendHtml]').click(function(e) {
+					editor.appendHtml('<strong>添加HTML</strong>');
+				});
+				K('input[name=clear]').click(function(e) {
+					editor.html('');
+				});
+			});
+		</script>	
+	
+</head>
+<body class="roomOpen">
+<div class="container">
+<h3>增加文章</h3>
+<form method="post" action="">
+<table width="100%" border="0" cellpadding="5" cellspacing="0">
+<tr>
+	<td width="15%" align="right">文章标题：</td>
+	<td>
+	<input type="text" name="article_title" value="" class="article_title" />
+	</td>
+</tr>
+
+<tr>
+	<td width="15%" align="right">文章分类：</td>
+	<td>
+	<select name="article_cate">
+		<option value="1">
+	长春古观</option>
+		<option value="2">
+	道教文化</option>
+		<option value="3">
+	关于城隍</option>
+		<option value="6">
+	&nbsp;&nbsp;&nbsp;城隍介绍</option>
+		<option value="7">
+	&nbsp;&nbsp;&nbsp;武汉城隍</option>
+		<option value="4">
+	道教资讯</option>
+		<option value="8">
+	&nbsp;&nbsp;&nbsp;新闻速递</option>
+		<option value="9">
+	&nbsp;&nbsp;&nbsp;宫观资讯</option>
+		<option value="10">
+	&nbsp;&nbsp;&nbsp;道教国际</option>
+		<option value="11">
+	&nbsp;&nbsp;&nbsp;宗教动态</option>
+		<option value="5">
+	联系我们</option>
+		</select>
+	</td>	
+</tr>
+
+<tr>
+	<td width="15%" align="right">文章是否展示：</td>
+	<td>
+	<select name="article_flag">
+	<option value="1" selected>展示</option>
+	<option value="0">不展示</option>
+	</select>
+	
+	</td>
+</tr>
+
+<tr>
+	<td width="15%" align="right">文章优化标题：</td>
+	<td>
+	<input type="text" name="article_headline" value="" class="article_title" />
+	</td>
+</tr>
+
+<tr>
+	<td width="15%" align="right">文章优化关键词：</td>
+	<td>
+	<input type="text" name="article_keywords" value="" class="article_title" />
+	</td>
+</tr>
+
+<tr>
+	<td width="15%" align="right">文章优化描述：</td>
+	<td>
+	<input type="text" name="article_description" value="" class="article_title" />
+	</td>
+</tr>
+
+<tr>
+	<td width="15%" align="right">文章内容：</td>
+	<td>
+	<textarea name="content" style="width:800px;height:400px;visibility:hidden;"></textarea>
+	</td>
+</tr>
+<tr>
+<td width="15%" align="right">&nbsp;</td>
+<td>&nbsp;</td>
+</tr>
+<tr>
+	<td width="15%" align="right">&nbsp;</td>
+	<td>&nbsp;&nbsp;<a href="" target="_top">放弃返回</a>&nbsp;&nbsp;<input type="submit" name="submit" value="提交" /></td>
+</tr>
+</table>
+
+</form>
+</div>
+
+<script type="text/javascript">
+$(document).ready(function(){
+	$("input[name='submit']").click(function(){
+		var article_title = $("input[name='article_title']").val();
+		var content = $("textarea[name='content']").val();
+		if(article_title == '')
+		{
+			alert("请输入标题！");
+			return false;
+		}
+		return true;
+	});
+	
+});
+</script>
+
+</body>
+</html>
